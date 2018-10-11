b78c5e6fd6769cb650a0df6c3fcb1b7ff6fbe44d
Brandon DePesa <bmdepesa@users.noreply.github.com>
2018-10-11 10:21:17 -0700

    Styling changes to stopRemoveAllContainers.sh

--- stopRemoveAllContainers.sh
+++ stopRemoveAllContainers.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo docker stop $(sudo docker ps -aq) && sudo docker rm $(sudo docker ps -aq) && sudo docker rmi $(sudo docker images -q)
+sudo docker stop $(sudo docker ps -aq)
+sudo docker rm $(sudo docker ps -aq)
+sudo docker rmi $(sudo docker images -q)
