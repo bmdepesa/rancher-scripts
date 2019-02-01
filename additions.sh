04cbf069d151afd04614cf8d0ecc05f03a0271eb
Brandon DePesa <brandon@rancher.com>
2019-02-01 04:30:26 -0700

    update

--- additions.sh
+++ additions.sh
@@ -25,7 +25,7 @@ function tugc {
   then 
     echo "Name is needed to create droplet!"
   else
-    tugboat create $1 --keys=19891744
+    tugboat create $1 --keys=<>
   fi
 }
 
@@ -38,15 +38,38 @@ function rll {
 }
 
 function agi_j {
-  filter="$1"
-  adopts="$2"
+  name_filter="$1"
+  additional_adopts="$2"
   aws ec2 describe-instances \
-  $adopts \
-  --filters "Name=tag:Name,Values=$filter" \
+  $additional_opts \
+  --filters "Name=tag:Name,Values=$name_filter" \
   --output json | \
-  jq -r '.Reservations[].Instances[] | select(.State.Name=="running") | { name: .Tags[] | select(.Key=="Name") | .Value, type: .InstanceType, id: .InstanceId, public: .PublicIpAddress, private: .PrivateIpAddress}'
+  jq -r '.Reservations[].Instances[] | select(.State.Name=="running") | { name: .Tags[] | select(.Key=="Name") | .Value, type: .InstanceType, id: .InstanceId, public: .PublicIpAddress, private: .PrivateIpAddress }'
 }
 
 function agi {
   agi_j $1 $2 | jq -r '"\(.name) \(.id) \(.type) \(.public) \(.private)"'
+}
+
+function aws_delete {
+  agi_j $1 $2 | jq -r '"\(.id)"' | while read p; do aws ec2 terminate-instances --instance-ids "$p"; done
+}
+
+function aci {
+  name=$1
+  size=$2
+  count=$3
+  ami=""
+  key=""
+  aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
+}
+
+function rancher_create_ha {
+  # create instances
+  # get instances
+  # add to target group
+  # create rke config
+  # rke up
+  # helm commands
+  # set kubeconfig
 }
